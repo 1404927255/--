a51b21a6c58d416978a823e9033d1367fcb9e7d0
1404927255 <115683268+1404927255@users.noreply.github.com>
2022-10-15 23:31:48 +0800

    Add files via upload

---
+++ 阶乘相加.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	int n, i;
+	int sum = 1;
+	int fax = 0;
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum * i;
+		printf("!%d=%d\n", i, sum);
+		fax += sum;
+	}
+	printf("%d", fax);
+
+}
