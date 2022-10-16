16965e3b90d6af5af064a62d71d2fc20145d5b3a
1404927255 <115683268+1404927255@users.noreply.github.com>
2022-10-16 16:02:42 +0800

    Add files via upload

---
+++ 两端向中间逼近.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<Windows.h>//这个函数库用来调用sleep，即间隔多少毫秒后在进行下次操作
+int main()
+{
+	char arr1[] = "i love you";//字符串后还会有一个\0，下标个数应该是元素个数-2
+	char arr2[] = "??????????";
+	int leaft = 0;
+	int right = strlen(arr2) - 1;//strlen使用后直接省略\0
+	while (leaft <= right)
+	{
+		arr2[leaft] = arr1[leaft];//即arr1和arr2相互替换
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(100);
+		leaft++;
+		right--;
+	}
+	return 0;
+}
---
+++ 使用2分法找数字.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };//给定一个有序整数数组，个数可以自己定
+	int a;
+	int sz = sizeof(arr) / sizeof(0);//sizeof计算这个数据的大小，arr/0就是整个数组的大小除以其中一个，就是这个数组的个数
+	int leaft, right, mid;//这里设定左下标和右下标，以及中间下标
+	leaft = 0;
+	right = sz - 1;
+	printf("请输入1-10的数字\n");
+	scanf_s("%d", &a);
+	while (leaft <= right)
+	{
+		mid = (leaft + right) / 2;
+		if (arr[mid] < a)//arr[mid]就是mid这个下标值在整个数组中对应的数字
+		{
+			leaft = mid + 1;
+		}
+		else if (arr[mid] > a)//和a进行比较，小的话左值变为mid+1，相反同理
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("这个数的下标是%d", mid);
+			break;
+		}
+	}
+	if (leaft > right)
+	{
+		printf("好像没有这个数字\n");
+	}
+}
