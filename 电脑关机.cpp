41ec3f4c0eddf17c7b79b63c33e682b67366f204
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-10-18 19:34:16 +0800

    Add files via upload

---
+++ 电脑关机.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[20] ;//定义这个数组时，不要放置内容，如果防止内容会使后面strcmp无法比较
+	system("shutdown -s -t 60");
+	printf("你的电脑将会在60秒后关机\n如果不想关机就输入：我是猪\n");
+	scanf_s("%s", &input ,10);
+	if(strcmp(input, "我是猪") == 0)
+	{
+		system("shutdown -a");
+	}
+
+	return 0;
+}
