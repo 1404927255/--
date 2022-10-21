dd2d63e8270812ebcf3e47728a6708033b71de22
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-10-21 21:06:22 +0800

    Add files via upload

---
+++ 冒泡排序.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#define P 1010
+int arr[P] = { 0 };
+int main()
+{
+	int n;
+	scanf_s("%d", &n);//n表示个数
+	for (int i = 1; i <= n; i++)//使用循环接收所有数
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (int i = 0; i < n - 1; i++)//i表示比较次数
+	{
+		for (int j = 0; j < n - i - 1 ; j++)//控制数组中对比数的下标
+		{
+			if (arr[j] > arr[j + 1])//依次两两比较如 1-2比，2-3比，3-4比，谁大谁放后面
+			{
+				int tem=arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tem;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)//到这里时，arr里面数已经排列好，依次将他们打出即可
+	{
+		printf("%d  ", arr[i]);
+	}
+	return 0;
+}
---
+++ 选择排序.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#define P 1010
+int arr[P] = { 0 };
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)//和冒泡同理
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = i + 1; j <= n  ; j++)//这里使j<=n，是为了让i和它后面所有数进行比较
+		{
+			if (arr[i] > arr[j])//如果i的大则和j换位置，将大数放在后面
+			{
+				int tem=arr[i];
+				arr[i] = arr[j];
+				arr[j] = tem;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d  ", arr[i]);
+	}
+	return 0;
+}
