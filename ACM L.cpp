753b6333e5a325fbe61872bb1a758ba5430280cb
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-10-31 11:27:49 +0800

    Add files via upload

---
+++ ACM L.cpp
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+#define p 1020
+int a[p][p], x[p][p];
+int n, k, i, j;
+int counter = 0;
+int main(void)
+{
+	scanf_s("%d %d", &n, &k);
+	for (i = 1; i <= n; i++)
+	{
+		int b, c, d, e;
+		scanf_s("%d %d %d %d", &b, &c, &d, &e);
+		a[b + 1][c + 1]++;
+		a[b + 1][e + 1]--;
+		a[d + 1][c + 1]--;
+		a[d + 1][e + 1]++;
+	}
+	for (i = 1; i <= 1000; i++)
+	{
+		for (j = 1; j <= 1000; j++)
+		{
+			x[i][j] = a[i][j] + x[i - 1][j] + x[i][j - 1] - x[i - 1][j - 1];
+			if (x[i][j] == k)
+			{
+				counter++;
+			}
+		}
+	}
+	printf("%d", counter);
+}
---
+++ ACM M.cpp
@@ -0,0 +1,36 @@
+﻿#include<stdio.h>
+#define p 1020
+int a[p][p], b[p][p];
+int t, k, i, j;
+int main(void)
+{
+	scanf("%d", &t);
+	while (t--)
+	{
+		int m, n, x, y, tem = 0;
+		scanf("%d %d %d %d", &m, &n, &x, &y);
+		if (x > m || y > n)
+		{
+			break;
+		}
+		for (i = 1; i <= m; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				scanf_s("%d", &a[i][j]);
+				b[i][j] = a[i][j] + b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
+			}
+		}
+		for (i = 1; i <= m - x + 1; i++)
+		{
+			for (j = 1; j <= n - y + 1; j++)
+			{
+				if (b[i + x - 1][j + y - 1] - b[i + x - 1][j - 1] - b[i - 1][j + y - 1] + b[i - 1][j - 1] > tem)
+				{
+					tem = b[i + x - 1][j + y - 1] - b[i + x - 1][j - 1] - b[i - 1][j + y - 1] + b[i - 1][j - 1];
+				}
+			}
+		}
+		printf("%d", tem);
+	}
+}
