aeb6a65d598b5a72a3b33638f50d9dc5af18edd9
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-01 23:11:46 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main(void)
+{
+	int n, m,sum;
+	cin >> n >> m;
+	sum = pow(n, m);
+	if (sum > 100)
+	{
+		sum %= 1000;
+		cout << sum;
+	}
+	else
+		cout<<0<<endl;
+}
---
+++ 2.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+int a[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+int b[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+int test(int year)
+{
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
+int main(void)
+{
+	int year, month, day;
+	cin >> year >> month >> day;
+	int tem, sum=0;
+	tem = test(year);
+	if (tem == 1)
+	{
+		for (int i = 1; i < month; i++)
+		{
+			sum += a[i];
+		}
+		cout << sum + day;
+	}
+	else
+	{
+		for (int i = 1; i < month; i++)
+		{
+			sum += b[i];
+		}
+		cout << sum + day;
+	}
+}
