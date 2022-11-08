066d5f965d12c40595a599bbda91803929af20e9
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-08 23:20:07 +0800

    Add files via upload

---
+++ H.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int arr[1000050];
+int n, m, r, counter = 0;
+int main()
+{
+    scanf_s("%d %d", &m, &n);
+    for (int i = 0; i < m; i++)
+    {
+        scanf_s("%d", &arr[i]);
+    }
+    sort(arr, arr + m);
+    while (n--)
+    {
+        scanf_s("%d", &r);
+
+        bool p;
+        p = binary_search(arr, arr + m, r);
+        if (p == true)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+}
