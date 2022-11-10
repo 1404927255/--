838c53d0ddf68f63f87157b26fe1c636d3d8815e
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-10 15:04:40 +0800

    Add files via upload

---
+++ ACM K 国王的魔镜.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+char arr[100001];
+int test(int right)
+{
+    for (int i = 1; i <= right / 2; i++)
+    {
+        if (arr[i] != arr[right - i + 1])
+        {
+            return 1;
+        }
+        else
+            return 0;
+    }
+}
+void test2(int right)
+{
+    if (right % 2 != 0)
+    {
+        printf("%d", right);
+        exit(0);
+    }
+    else
+    {
+        int tem = test(right);
+        if (tem == 1)
+        {
+            printf("%d", right);
+            exit(0);
+        }
+        test2(right / 2);
+    }
+}
+int main(void)
+{
+    scanf_s("%s", arr + 1);
+    int right = strlen(arr + 1);
+    test2(right);
+}
