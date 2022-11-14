80ffc4ec903a739ce801769b32b2fc1921db5e9a
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-14 23:24:13 +0800

    Add files via upload

---
+++ 寻找素数对.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+
+int test(int a)
+{
+	for (int i = 2; i < a; i++)
+	{
+		if (a % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+int main()
+{
+	int n = 0;
+	while (scanf_s("%d", &n) != EOF)
+	{
+		if (n < 5 || n % 2 == 1)
+			exit(0);
+		int p, q;
+		q = p = n / 2;
+		while (test(p) == 0 || test(q) == 0)
+		{
+			q--;
+			p++;
+		}
+		printf("%d %d\n", q, p);
+	}
+	return 0;
+}
