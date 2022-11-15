11e5cc073ed6956be9225ab447a6c0ff4d246c59
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-15 22:05:39 +0800

    Add files via upload

---
+++ J.统计素数.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+int test(int a)
+{
+	if (a == 2)
+		return 1;
+	else if (a < 2)
+		return 0;
+	for (int i = 2; i <= sqrt(1.0 * a); i++)
+	{
+		if (a % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+int main()
+{
+	int n, m;
+	while (scanf_s("%d", &n)!=EOF)
+	{
+		int counter = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			scanf_s("%d", &m);
+			if (test(m))
+			{
+				counter++;
+			}
+		}
+		printf("%d\n", counter);
+	}
+	return 0;
+}
