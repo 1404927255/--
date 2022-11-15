e4e4d74c22e7a7ce7afb51219f473d591cf60fae
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-15 22:06:16 +0800

    Add files via upload

---
+++ C.分拆素数和.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+int test(int a)
+{
+	if (a < 2)
+		return 0;
+	for (int i = 2; i <= floor(sqrt(a)); i++)
+	{
+		if (a % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+int main()
+{
+	int n = 0;
+	int counter = 0;
+	while (scanf_s("%d", &n) != EOF)
+	{
+		if (n % 2 == 1 || n == 0)
+			exit(0);
+		for (int i = 2; i < n / 2; i++)
+		{
+			if (test(i) == 1 && test(n - i) == 1)
+			{
+				counter++;
+			}
+		}
+		printf("%d\n", counter);
+		counter = 0;
+	}
+	return 0;
+}
---
+++ D.素数判断.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+
+int test(int a)
+{
+	int tem = 0;
+	tem = a * a + a + 41;
+	return tem;
+}
+
+int test2(int a)
+{
+	if (a < 2)
+		return 0;
+	else if (a == 2)
+		return 1;
+	for (int i = 2; i <= floor(sqrt(a)); i++)
+	{
+		if (a % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int n, m;
+	while(scanf_s("%d %d", &n, &m)!=EOF)
+	{
+		int tem = 0;
+	if (n>m|| n == m)
+		exit(0);
+	for (int i = n; i <= m; i++)
+	{
+		if (!test2(test(i)))
+		{
+			tem = 1;
+			break;
+		}
+	}
+	if (tem == 1)
+	{
+		printf("Sorry\n");
+	}
+	else
+		printf("OK\n");
+	}
+	return 0;
+}
