2d32702212444f1ae76a46097163229aca84bdcf
Tanjy <115683268+1404927255@users.noreply.github.com>
2022-11-17 14:34:21 +0800

    Add files via upload

---
+++ I最大因质数.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+
+int a[1000001];
+int main()
+{
+    int cnt = 1,n;
+    memset(a, 0, sizeof(a));
+    for (int i = 2; i < 1000001; i++)
+    {
+        if (a[i] == 0)
+        {
+            a[i] = cnt;
+            for (int k = 2 * i; k < 1000001; k = k + i)
+            {
+                a[k] = cnt;
+            }
+            cnt++;
+        }
+    }
+    while (scanf("%d", &n) != EOF)
+    {
+        printf("%d\n", a[n]);
+    }
+    return 0;
+}
